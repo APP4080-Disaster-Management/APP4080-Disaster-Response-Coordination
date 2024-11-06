c2400976aceb26a6c942117202dde4512473881b
Martha-Iradukunda <marthairadukunda1@gmail.com>
2024-11-06 18:03:14 +0300

    Test4

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,5 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/disasters', disasterRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
